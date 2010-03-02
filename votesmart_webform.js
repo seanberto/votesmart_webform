12ec2d92f995adbe1179815568e2bbc393cdefa7
Troy Thompson <troynt@gmail.com>
2010-03-02 04:06:34 +0000

    making edit screens behave properly

--- votesmart_webform.js
+++ votesmart_webform.js
@@ -1,3 +1,28 @@
+//update candidate checkbox
+function votesmart_webform_update_candidate( $candidate, $selected_candidates )
+{
+  var $t = $candidate;
+
+  var selected_id = 'selected-'+ $t.attr('name').match(/\d+/)[0];
+  var label = $t.parent().text();
+
+  if( $t.filter(':checked').length == 1 )
+  {
+    $selected_candidate = $('<a href="#" title="uncheck candidate" id="'+ selected_id+'">'+ label +'; </a>');
+    var that = this;
+    $selected_candidate.click(function(){
+      $(that).attr('checked','');
+      $(this).remove();
+      return false;
+    });
+    $selected_candidates.children().length == 0
+    $selected_candidates.append($selected_candidate);
+  }
+  else
+    $selected_candidates.children('#'+selected_id).trigger('click');
+
+}
+
 function votesmart_webform_candidate_wrapper( $candidate_wrapper )
 {
     if( $candidate_wrapper.length < 1 ) return;
@@ -9,25 +34,9 @@ function votesmart_webform_candidate_wrapper( $candidate_wrapper )
 
     $candidate_wrapper.after($selected_candidates_wrapper);
     $('input[type=checkbox]').change(function(){
-      var $t = $(this);
-
-      var selected_id = 'selected-'+ $t.attr('name').match(/\d+/)[0];
-      var label = $t.parent().text();
-
-      if( $t.filter(':checked').length == 1 )
-      {
-        $selected_candidate = $('<a href="#" title="uncheck candidate" id="'+ selected_id+'">'+ label +'; </a>');
-        var that = this;
-        $selected_candidate.click(function(){
-          $(that).attr('checked','');
-          $(this).remove();
-          return false;
-        });
-        $selected_candidates.children().length == 0
-        $selected_candidates.append($selected_candidate);
-      }
-      else
-        $selected_candidates.children('#'+selected_id).trigger('click');
+      votesmart_webform_update_candidate($(this),$selected_candidates);
+    }).each(function(){
+      votesmart_webform_update_candidate($(this),$selected_candidates);
     });
 
 
@@ -39,8 +48,18 @@ jQuery(function(){
     $component = $(this);
 
     $zip = $component.find('input[name~=zip]');
-    if( $zip.length == 1 && $zip.val().match(/\d+/) )
+
+    //if the zip field has a value and we don't have any candidates
+    //trigger change so we can search for candidates
+    if( $zip.length == 1
+        && $zip.val().match(/\d+/)
+        && $component.find('.votesmart-webform-candidate-wrapper').length < 1
+    )
+    {
       $zip.trigger('change');
+    }
+
+
 
     $candidate_wrapper = $component.find('.votesmart-webform-candidate-wrapper');
     window.setTimeout(function(){
